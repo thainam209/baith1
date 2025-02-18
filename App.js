349247dfff4e8904aad8846ccdc2f1897baf73e7
thainam209 <thainam2092004@gmail.com>
2025-02-18 10:10:37 +0700

    update

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { BackHandler, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validatePhoneNumber = (phone) => {
+    // Biểu thức Regex kiểm tra số điện thoại Việt Nam
+    const phoneRegex = /^(0[3|5|7|8|9])+([0-9]{8})$/;
+
+    if (phoneRegex.test(phone)) {
+      setErrorMessage('Số điện thoại hợp lệ!');
+
+      
+    } else {
+      setErrorMessage('Số điện thoại không hợp lệ!');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -10,9 +27,20 @@ export default function App() {
       <Text style={styles.title1}>Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro</Text>
       <TextInput style={styles.input}
       placeholder="Nhập số điện thoại của bạn"
-      keyboardType="phone-pad">  
+      keyboardType="numeric"
+      value={phoneNumber}
+      onChangeText={setPhoneNumber}
+      >  
       </TextInput>
-      <Button title='Tiếp tục' ></Button>
+      {errorMessage ? (
+        <Text style={errorMessage.includes('Số điện thoại hợp lệ!') ? styles.successMessage : styles.errorMessage}>
+          {errorMessage}
+        </Text>
+      ) : null}
+      <Text></Text>
+      <Text style={styles.button}
+        onPress={() => validatePhoneNumber(phoneNumber)}
+      >Tiếp tục</Text>
     </View>
   );
 }
@@ -50,9 +78,29 @@ const styles = StyleSheet.create({
     height: 100,
     fontSize : 20 ,
     borderBottomWidth:2,
-    borderColor: '#efefef'
+    borderColor: '#efefef',
+    marginBottom: 30
   },
   button: {
-    
+    alignContent: 'center',
+    borderRadius: 8,
+    alignItems: 'center',
+    backgroundColor: '#000ef8',
+    paddingLeft:161,
+    paddingTop:10,
+    height:60,
+    width:400,
+    marginLeft: 17,
+    color:'white',
+    paddingTop:18,
+    fontSize: 20
+  },
+  successMessage: {
+    color: 'green',
+    paddingLeft:25
+  },
+  errorMessage: {
+    color: 'red',
+    paddingLeft:25
   }
 });
